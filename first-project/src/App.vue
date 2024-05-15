ef7e5bcb6d942db8e4e96b961612a54ea55dd001
Gabriella Fadel <gabriellafadelbraz@outlook.com>
2024-05-14 21:08:58 -0300

    first project - logo - vue structure

--- first-project/src/App.vue
+++ first-project/src/App.vue
@@ -1,10 +1,11 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <img alt="Vue logo" src="./assets/1.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
